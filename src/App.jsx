2e33347218b60a04049bbd280875eeed005a150d
Your Name <you@example.com>
2025-04-18 20:44:20 +0500

    added featureSection, heroSection, Workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Workflow from "./components/Workflow";
+import FeatureSection from "./components/FeatureSection";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
     return (
         <>
             <Navbar />
-            <div className="max-w-7xl mx-auto pt-20 px-6"></div>
+            <div className="max-w-7xl mx-auto pt-20 px-6">
+                <HeroSection />
+                <FeatureSection/>
+                <Workflow />
+            </div>
         </>
     );
 };

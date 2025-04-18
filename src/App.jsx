b312aab6eb0691b76b27770a7ea00e3a12b8b962
Danil <makarovdanil289@gmail.com>
2025-04-18 19:00:11 +0500

    Initial commit and add Navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="max-w-7xl mx-auto pt-20 px-6"></div>
+        </>
+    );
+};
+
+export default App;
